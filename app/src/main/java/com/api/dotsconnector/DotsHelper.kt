871d5f1fba590b42c4be9dd14ff0c42254f6b4b2
AngelsPearlInfotech <angelspearlit@gmail.com>
2018-05-28 14:24:44 +0530

    Merge branch 'master' of https://github.com/AngelsPearlInfotech/DotsConnector
    # Conflicts:
    #	app/src/main/java/com/api/dotsconnector/DotsHelper.kt

--- app/src/main/java/com/api/dotsconnector/DotsHelper.kt
+++ app/src/main/java/com/api/dotsconnector/DotsHelper.kt
@@ -5,10 +5,7 @@ class DotsHelper{
     var top=false
     var right = false
     var bottom = false
-
     var allset = false
-    
-
     var jhgjh=false
     var righttobottom=false
     var righttobottom1=false
